b80facf51cfc0fe594dcd21d8079ea71dcac9926
Gabriela Piovesan Plinski <gabrielaplinski@outlook.com>
2025-11-11 23:29:55 -0300

    js/styles update

--- script/script.js
+++ script/script.js
@@ -1,3 +1,40 @@
-function addToCart() {
-    console.log("Item added to cart")
+const cart = [];
+const cartList = document.querySelector("#cart-items");
+const cartTotal = document.querySelector("#cart-total");
+const emptyCart = document.querySelector("#empty-cart");
+
+document.querySelectorAll(".addTo").forEach(button => {
+    button.addEventListener("click", () => {
+        const name = button.dataset.name;
+        const price = parseFloat(button.dataset.price);
+
+        const existingItem = cart.find(item => item.name === name);
+        if (existingItem) {
+            existingItem.quantity++;
+        } else {
+            cart.push({ name, price, quantity: 1});
+        }
+
+        updateCart();
+    });
+});
+
+function updateCart() {
+    cartList.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        total += item.price * item.quantity;
+
+        const li = document.createElement("li");
+        li.textContent = `${item.name} ${item.quantity}x @$${item.price.toFixed(2)} $${(item.price * item.quantity).toFixed(2)}`;
+        cartList.appendChild(li);
+    });
+    cartTotal.textContent = `Order Total $${total.toFixed(2)}`;
+
+    if (cart.length > 0) {
+        emptyCart.style.display = "none";
+    } else {
+        emptyCart.style.display = "block";
+    }
 }
